c0e28e419327cf9a3245607a3104c11d51079bd9
oncomouse <oncomouse@gmail.com>
2022-01-22 11:08:58 -0600

    Have to wrap Button's test in ThemeProvider

--- src/components/Button.spec.js
+++ src/components/Button.spec.js
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 import '@testing-library/jest-dom'
 import Button from './Button.js'
 import { store } from '../features/store'
+import theme from '../features/theme'
 
 describe('<Button /> Component', () => {
   test('Renders', () => {
     render(
       <Provider store={store}>
-        <Button actionCreator={() => {}}>Click Me</Button>
+        <ThemeProvider theme={theme}>
+          <Button actionCreator={() => {}}>Click Me</Button>
+        </ThemeProvider>
       </Provider>
     )
     expect(screen.getByText(/Click Me/i)).toBeInTheDocument()
